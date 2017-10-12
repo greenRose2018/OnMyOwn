8d44f530e386e86d90d99e8826cba52571fdf11d
greenRose2018 <brifun13@gmail.com>
2017-10-12 09:17:48 -0600

    Made a new screen

---
+++ OnMyOwn/Controller/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  OnMyOwn
+//
+//  Created by Morales, Brittney on 10/12/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class SecondViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
